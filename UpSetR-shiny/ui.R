ccd52df55ab1bdc1e374887147c03ed77c8d8b4d
JakeConway <jakeconway45@gmail.com>
2015-07-14 17:07:33 -0400

    Fixed #3

--- UpSetR-shiny/ui.R
+++ UpSetR-shiny/ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("UpSet R"),
+shinyUI(navbarPage("UpSet R",
+ tabPanel("Instructions",
   sidebarLayout(
     sidebarPanel(
       fluidRow(
@@ -15,38 +15,38 @@ shinyUI(fluidPage(
         radioButtons('sep', label = h6('Separator'),
                      choices = c(Comma=',', Semicolon =';', Tab='\t'),
                      selected = ';')
-      ),
-      fluidRow(
-        numericInput("nintersections", label = h6("Number of Intersections"), value = 40, min = 1, max = 60)
-      ),
-      fluidRow(
-        htmlOutput("sets")
-      ),
-      fluidRow(
-        sliderInput("mbratio", label = h6("Proportion of Matrix Plot"), value = 0.30, min = 0.20, max = 0.80,
-                    ticks = FALSE) 
-      ),
-      fluidRow(
-        numericInput("pointsize", label = h6("Point Size"), value = 4, min = 1, max = 15)
-      ),
-      fluidRow(
-        selectInput("order", label = h6("Order by"), choices = list("Degree" = "degree",
-                                                                    "Frequency" = "freq",
-                                                                    "Degree then Frequency" = "degfreq"),
-                    selected = "freq")),
-      fluidRow(
-        radioButtons(inputId = "filetype", label = "Select the file type", choices = list("png", "pdf"))
-        ),
-      fluidRow(
-        downloadButton(outputId = "down", label = "Download the plot!")
-        )
-      ,width = 2),
-    mainPanel( tabsetPanel(
-      tabPanel("Your Data", tableOutput('data'),
-               textOutput('obs')
-      ),
-      tabPanel("UpSet Plot", imageOutput('plot'))
-    ), width = 10
-    )
-  )
-))
+      ),width = 2), mainPanel())),
+ tabPanel("Your Data",
+          mainPanel( tableOutput('data'),
+                     textOutput('obs'), width = 10
+          )),
+      tabPanel("UpSet Plot",
+               sidebarLayout(
+                 sidebarPanel(
+                   fluidRow(
+                     numericInput("nintersections", label = h6("Number of Intersections"), value = 40, min = 1, max = 60)
+                   ),
+                   fluidRow(
+                     htmlOutput("sets")
+                   ),
+                   fluidRow(
+                     sliderInput("mbratio", label = h6("Proportion of Matrix Plot"), value = 0.30, min = 0.20, max = 0.80,
+                                 ticks = FALSE) 
+                   ),
+                   fluidRow(
+                     numericInput("pointsize", label = h6("Point Size"), value = 4, min = 1, max = 15)
+                   ),
+                   fluidRow(
+                     selectInput("order", label = h6("Order by"), choices = list("Degree" = "degree",
+                                                                                 "Frequency" = "freq",
+                                                                                 "Degree then Frequency" = "degfreq"),
+                                 selected = "freq")),
+                   fluidRow(
+                     radioButtons(inputId = "filetype", label = "File type", choices = list("png", "pdf"))
+                   ),
+                   fluidRow(
+                     downloadButton(outputId = "down", label = "Download!")
+                   )
+                   ,width = 2),mainPanel(imageOutput('plot')
+                                         ,width = 10))
+  )))
