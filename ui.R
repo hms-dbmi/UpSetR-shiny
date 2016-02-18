58c235bb2a11622dc564fe12c55d00db50d318fa
JakeConway <jakeconway45@gmail.com>
2016-02-18 15:48:28 -0500

    Rearrange side panel and deploy app

--- ui.R
+++ ui.R
@@ -192,14 +192,14 @@ shinyUI(navbarPage(title = "",
                                  selected = "dec")
                    ),
                    fluidRow(
+                     sliderInput("mbratio", label = h6("Bar : Matrix ratio"), value = 0.30, min = 0.20, max = 0.80,
+                                 ticks = FALSE, step = 0.01) 
+                   ),
+                   fluidRow(
                      sliderInput("angle", h6("Number angles"),  min = -90, max = 90, value = 0, step = 1, ticks = F)
                    ),
                    fluidRow(
                      checkboxInput('empty', label = "Show empty intersections", value = FALSE)
-                     ),
-                   fluidRow(
-                     sliderInput("mbratio", label = h6("Bar : Matrix ratio"), value = 0.30, min = 0.20, max = 0.80,
-                                 ticks = FALSE, step = 0.01) 
                    ),
                    fluidRow(
                      numericInput("pointsize", label = h6("Point Size"), value = 4, min = 1, max = 15)
